d735e8caadec9d4a685641649e7ba95db84f0f56
--global <--global>
2021-02-25 10:19:07 +0100

    feat: Merge sort.
    Adds python implementation of the merge sort algorithm.

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -12,6 +12,47 @@ class Algorithms(object):
                     input[idx+1] = tmp
         return input
 
+    def merge(self, left, right):
+        output = []
+        while len(left) > 0 and len(right) > 0:
+            if left[0] <= right[0]:
+                output.append(left[0])
+                left = left[1:]
+            else:
+                output.append(right[0])
+                right = right[1:]
+
+        while len(left) > 0:
+            output.append(left[0])
+            left = left[1:]
+
+        while len(right) > 0:
+            output.append(right[0])
+            right = right[1:]
+            
+        return output
+
+
+    def merge_sort(self, input):
+
+        if len(input) <= 1:
+            return input
+
+        left = []
+        right = []
+
+        for idx in range(0, len(input)):
+            if idx < len(input) / 2:
+                left.append(input[idx])
+            else:
+                right.append(input[idx])
+
+        left = self.merge_sort(left)
+        right = self.merge_sort(right)
+
+        return self.merge(left, right)
+
+
 
 
 
---
+++ algorithms/test_merge_sort.py
@@ -0,0 +1,7 @@
+import pytest
+from algorithms import Algorithms
+
+def test_merge_sort():
+    input = [7,2,8,9,0,3,5,1,4,6]
+    result = Algorithms().merge_sort(input)
+    assert(result == [0,1,2,3,4,5,6,7,8,9])
