4cf1812ba74693f7d8a4406c6907d6eba2615aa4
Andrew Presland <andrew.presland@gmail.com>
2021-02-26 10:52:39 +0100

    refactor: Use std::swap to swith values in merge sort implementation

--- cpp/src/algorithms.cpp
+++ cpp/src/algorithms.cpp
@@ -6,18 +6,6 @@
 
 void bubble_sort(std::vector<int> &input)
 {
-    /**
-    for( int step = input.size()-1; step >= 0; --step) {
-        for( int j = 0; j < step ; ++j) {
-            if(input.at(j) > input.at(j+1)) {
-                int tmpVal = input.at(j);
-                input.at(j) = input.at(j+1);
-                input.at(j+1) = tmpVal;
-            }
-        }
-    }
-    */
-
     std::vector<int>::iterator begin = std::begin(input);
     std::vector<int>::iterator end = std::end(input);
     
@@ -25,9 +13,10 @@ void bubble_sort(std::vector<int> &input)
         for( auto i = begin; i != step ; ++i) {
             std::cout << *i << "," << *(i+1) << std::endl;
             if(*i > *(i+1)) {
-                int tmpVal = *i;
-                *i = *(i+1);
-                *(i+1) = tmpVal;
+                //int tmpVal = *i;
+                //*i = *(i+1);
+                //*(i+1) = tmpVal;
+                std::swap(*(i+1),*i);
             }
         }
     }
