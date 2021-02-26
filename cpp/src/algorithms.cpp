eec374ae5124c615c62b37bf9746a56966477c65
Andrew Presland <andrew.presland@gmail.com>
2021-02-26 10:47:45 +0100

    refactor: Replace 'classic' style loops with 'modern' iterators

--- cpp/src/algorithms.cpp
+++ cpp/src/algorithms.cpp
@@ -6,12 +6,28 @@
 
 void bubble_sort(std::vector<int> &input)
 {
-    for( int step = input.size()-1; step > -1; --step) { 
+    /**
+    for( int step = input.size()-1; step >= 0; --step) {
         for( int j = 0; j < step ; ++j) {
             if(input.at(j) > input.at(j+1)) {
                 int tmpVal = input.at(j);
                 input.at(j) = input.at(j+1);
                 input.at(j+1) = tmpVal;
+            }
+        }
+    }
+    */
+
+    std::vector<int>::iterator begin = std::begin(input);
+    std::vector<int>::iterator end = std::end(input);
+    
+    for( auto step = end; step != begin; --step) {
+        for( auto i = begin; i != step ; ++i) {
+            std::cout << *i << "," << *(i+1) << std::endl;
+            if(*i > *(i+1)) {
+                int tmpVal = *i;
+                *i = *(i+1);
+                *(i+1) = tmpVal;
             }
         }
     }
